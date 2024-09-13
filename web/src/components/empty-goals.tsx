5f939f0ff39ba0985912b6cbf4733ba332f19f49
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-09-13 00:52:30 -0300

    finish: web ui

---
+++ web/src/components/empty-goals.tsx
@@ -0,0 +1,24 @@
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+import logo from '../assets/logo-task.svg'
+import letsStartIllustration from '../assets/lets-start-illustration.svg'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="task" />
+      <img src={letsStartIllustration} alt="task" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
